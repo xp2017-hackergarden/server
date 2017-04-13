310569024a418c03276a4a27ca320592ac6fe9d5
ognjetina <ognjen.lazic@codecentric.de>
2017-04-13 09:36:53 +0200

    Fix root test

--- xpserver_web/tests/unit/test_web.py
+++ xpserver_web/tests/unit/test_web.py
@@ -1,9 +1,8 @@
 from django.core.urlresolvers import resolve
-import pytest
 from xpserver_web.views import hello_world
 
 
 def test_root_resolves_to_hello_world():
-    found = resolve('/web/')
+    found = resolve('/')
     assert found.func == hello_world
 
