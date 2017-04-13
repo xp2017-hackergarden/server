ee5ab61090cef682f37631a8c3f5764bdda63772
ognjetina <ognjen.lazic@codecentric.de>
2017-04-13 14:36:37 +0200

    Add unit test for register

--- xpserver_web/tests/unit/test_web.py
+++ xpserver_web/tests/unit/test_web.py
@@ -1,8 +1,12 @@
 from django.core.urlresolvers import resolve
-from xpserver_web.views import main
+from xpserver_web.views import main, register
 
 
-def test_root_resolves_to_hello_world():
+def test_root_resolves_to_main():
     found = resolve('/')
     assert found.func == main
 
+
+def test_register_resolves_to_main():
+    found = resolve('/register/')
+    assert found.func == register
