3f0c84b82631eeae84cc829da4788d348b828e36
milansusnjar-cc <milan.susnjar@codecentric.de>
2017-04-19 11:35:42 +0200

    Test removed.

--- xpserver_api/tests/unit/test_api.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from xpserver_api.views import hello_world
-
-def test_hello_world(client):
-    response = client.get('/api/hello_world')
-    assert response.content.decode('utf-8') == 'hello world'
