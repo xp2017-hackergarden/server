e082ac44388cc05ceac8374d01e5ff3e1657ecfd
ognjetina <ognjen.lazic@codecentric.de>
2017-04-20 14:33:46 +0200

    Add exception rise if email is blank
    If registraiton via api gets blank email it will raise exception.

--- xpserver_api/serializers.py
+++ xpserver_api/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers, viewsets
 from xpserver_api.permissions import IsCreationOrIsAuthenticated
 from xpserver_api.services import generate_activation_code, EmailSender
 from xpserver_web.models import Profile
+from rest_framework.exceptions import APIException
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -11,16 +12,19 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'email')
 
     def create(self, validated_data):
-        user = User.objects.create(**validated_data)
         email = validated_data['email']
-        user.username = email
-        user.is_active = False
-        user.save()
-        profile = Profile.objects.create(user=user, activation_code=generate_activation_code())
-        profile.save()
-        email_sender = EmailSender()
-        email_sender.send_activation_email_with(profile=profile)
-        return user
+        if email:
+            user = User.objects.create(**validated_data)
+            user.username = email
+            user.is_active = False
+            user.save()
+            profile = Profile.objects.create(user=user, activation_code=generate_activation_code())
+            profile.save()
+            email_sender = EmailSender()
+            email_sender.send_activation_email_with(profile=profile)
+            return user
+        else:
+            raise APIException("Enter a valid email address.")
 
 
 class UserViewSet(viewsets.ModelViewSet):
